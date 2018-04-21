8777f8bb4020dcbbc56ca7eb9369d4d21ee6aba9
DownToSky <mme94@scarletmail.rutgers.edu>
2018-04-21 11:19:24 -0400

    notifies at run

--- background.js
+++ background.js
@@ -1,5 +1,19 @@
+
+var opt = {
+  type: "basic",
+  title: "notifications!",
+  message: "testing",
+  iconUrl: "icon19.png"
+}
+chrome.notifications.create(opt, callback);
+function callback(){
+	console.log("Popup don!");
+}
+
+/*
 chrome.tabs.query({}, function printTitles(tabs){
 	for(i = 0; i<tabs.length; i++){
 		console.log(tabs[i].title);
 	}
 });
+*/
