ad1abd29a02d74cb846978360ad978d2af8d4975
Hedwig <mme94@scarletmail.rutgers.edu>
2018-04-24 16:55:36 -0400

    tab checking
    Tab checking is being done. Incomplete.

--- background.js
+++ background.js
@@ -1,22 +1,46 @@
-alert("Background.js is now running!/");
+var triviaCategories = ["Quotes", "Titles", "Skills", "Lore", "LCS", "Summoner Spells", "Designers", "voice Actors"];
 
-/*
-var opt = {
-  type: "basic",
-  title: "notifications!",
-  message: "testing",
-  iconUrl: "icon19.png"
-}
-chrome.notifications.create(opt, callback);
-function callback(){
-	console.log("Popup don!");
-}
-*/
-
-/*
-chrome.tabs.query({}, function printTitles(tabs){
+var tabStore = {};
+chrome.tabs.query({}, function(tabs){
 	for(i = 0; i<tabs.length; i++){
-		console.log(tabs[i].title);
+		var tabId = tabs[i].tabId;
+		tabStore[tabId] = tabs[i];
 	}
 });
-*/
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    tabStore[tabId] = tab;
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId) {
+    delete tabStore[tabId];
+});
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+	if (activeInfo.tabId in tabStore)
+	{
+		var opt = {
+		  type: "basic",
+		  title: "notifications!",
+		  message: tabStore.tabId.title,
+		  iconUrl: "icon19.png"
+		}
+
+		function callback(){
+			console.log("Popup don!");
+		}
+
+		chrome.notifications.create(opt, callback);
+	}
+	else
+	{
+		console.log(activeInfo.tabId + " not in " + Object.keys(tabStore).join(", "));
+	}
+});
+
+
+
+
+
+
+
